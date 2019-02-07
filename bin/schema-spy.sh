0f16009068633fdb13e11314151297705ca8bbf0
Jeremy Sells <jeremy.sells@qikserve.com>
2019-02-07 17:51:21 +0000

    Adding Schema Spy

---
+++ bin/schema-spy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source "${DIR}/../.env"
+
+COMMAND=$@
+
+docker run \
+    --rm \
+    --user $(id -u):$(id -g) \
+    --volume /etc/passwd:/etc/passwd:ro \
+    --volume /etc/group:/etc/group:ro \
+    --volume ${PWD}:/${PWD} \
+    --workdir="${PWD}" \
+    schemaspy/schemaspy:${SCHEMASPY_VERSION} \
+    ${COMMAND}
