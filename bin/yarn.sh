bcc6eddead2e187a5d9ae9af35fdbb1d53538ba1
Jeremy Sells <mrjeremysells@gmail.com>
2019-03-07 21:52:26 +0000

    Yarn is included in the Node container, using this instead of building my own

--- bin/yarn.sh
+++ bin/yarn.sh
@@ -6,18 +6,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "${DIR}/../.env"
 
 COMMAND=$@
-export YARN_UBUNTU_VERSION=${YARN_UBUNTU_VERSION}
+export YARN_NODE_VERSION=${YARN_NODE_VERSION}
 
-docker-compose \
-    --file "${DIR}/../docker-compose.yml" \
-    --project-name ${PROJECT_NAME} \
-    run \
-        --rm \
-        --user $(id -u):$(id -g) \
-        --volume /etc/passwd:/etc/passwd:ro \
-        --volume /etc/group:/etc/group:ro \
-        --volume ${PWD}:/${PWD} \
-        --workdir="${PWD}" \
+docker run \
+    -it \
+    --rm \
+    --user $(id -u):$(id -g) \
+    --volume /etc/passwd:/etc/passwd:ro \
+    --volume /etc/group:/etc/group:ro \
+    --env HOME=$HOME \
+    --volume ${PWD}:/${PWD} \
+    --workdir="${PWD}" \
+    --network host \
+    node:${YARN_NODE_VERSION} \
         yarn \
-            yarn \
             ${COMMAND}
