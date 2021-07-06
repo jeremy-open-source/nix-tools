791a4edc90974a7e2a4a121f56289e7a717ed242
Jeremy Sells <mrjeremysells@gmail.com>
2021-07-06 13:45:11 +1200

    Script to decrypt PDFs

---
+++ bin/pdf-decrypt.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+IN_FILE=${1}
+PASSWORD=${2}
+OUT_FILE=${3}
+
+if [[ -z "${IN_FILE}" ]] ; then
+  read -r -p "Input File :" IN_FILE
+fi
+if [[ -z "${PASSWORD}" ]] ; then
+  read -r -p "Password :" PASSWORD
+fi
+if [[ -z "${OUT_FILE}" ]] ; then
+  read -r -p "Output File :" OUT_FILE
+fi
+
+# pdftk "${IN_FILE}" input_pw "${PASSWORD}" output "${OUT_FILE}"
+qpdf --password="${PASSWORD}" --decrypt "${IN_FILE}" "${OUT_FILE}"
+
