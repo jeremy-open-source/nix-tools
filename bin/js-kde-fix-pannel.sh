6aca2be8b7370d933ce1def8e74e072dc1d44ded
Jeremy Sells <mrjeremysells@gmail.com>
2021-09-06 19:59:34 +1200

    Fixing script

--- bin/js-kde-fix-pannel.sh
+++ bin/js-kde-fix-pannel.sh
@@ -4,6 +4,6 @@ set -e
 
 # Fixes issues with the KDE panel disappearing (had a couple of times in Kubuntu 21.04)
 
-rm ~/.config/plasma-org.kde.plasma.desktop-appletrc
+rm ~/.config/plasma-org.kde.plasma.desktop-appletrc || true
 
 kquitapp5 plasmashell && kstart5 plasmashell &
