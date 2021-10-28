dc033d2d452fcf61d1d03dc5bab353a7b2fade54
Jeremy Sells <mrjeremysells@gmail.com>
2021-10-28 13:51:02 +1300

    Improving script

--- bin/js-pyenv-create.sh
+++ bin/js-pyenv-create.sh
@@ -20,32 +20,33 @@ function wish_to_continue {
 
 # VERSION="3.6.14"  # Ubuntu 18.04
 # VERSION="3.8.11"
-VERSION="3.9.6"
+VERSION=${1-"3.9.6"}
 
 PROJECT_NAME=$(basename "$PWD")
 # Versions = 'pyenv versions'
-COMMAND_1="pyenv virtualenv \"${VERSION}\" \"${PROJECT_NAME}\""
-COMMAND_2="pyenv activate \"${PROJECT_NAME}\""
-COMMAND_3="pip install pip-tools"
+COMMAND_1="pyenv install \"${VERSION}\" || true"
+COMMAND_2="pyenv virtualenv \"${VERSION}\" \"${PROJECT_NAME}\""
+COMMAND_3="pyenv activate \"${PROJECT_NAME}\""
+COMMAND_4="pip install pip-tools"
 
 echo "INFO: I will run:"
 echo "INFO: '${COMMAND_1}'"
 echo "INFO: '${COMMAND_2}'"
 echo "INFO: '${COMMAND_3}'"
+echo "INFO: '${COMMAND_4}'"
 wish_to_continue
 
 eval ${COMMAND_1}
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 eval ${COMMAND_2}
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 eval ${COMMAND_3}
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+eval ${COMMAND_4}
 
-echo "Please run the following command if it fails!:"
-echo "* 'pyenv install ${VERSION}'"
 echo "Please run the following commands:"
-echo "* '${COMMAND_2}'"
+echo "* '${COMMAND_3}'"
 # echo "* 'pip install pip-tools'"
 
 # Note: To remove
