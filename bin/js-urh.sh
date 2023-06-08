856f25a33a8d30aa34229094cdb678898263147b
Jeremy Sells <mrjeremysells@gmail.com>
2023-06-08 14:40:24 +1200

    New scripts

--- bin/js-urh.sh
+++ bin/js-urh.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+IMAGE="jopohl/urh"
+
 sudo xhost +local:docker
 
-sudo docker run --rm        -e DISPLAY=${DISPLAY}        -v /tmp/.X11-unix:/tmp/.X11-unix        -v $HOME:/root        --privileged        jopohl/urh
+docker pull ${IMAGE}
+
+sudo docker run --rm \
+  -e DISPLAY=${DISPLAY} \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $HOME:/root \
+  --privileged \
+  ${IMAGE}
